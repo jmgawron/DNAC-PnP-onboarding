26f8a97e839cd979591dc0368e2f9dec7db533c2
Jaroslaw Gawron <jaroslaw.gawron@gmail.com>
2019-02-19 17:33:34 +0100

    first commit

---
+++ dnac_config.py
@@ -0,0 +1,5 @@
+import os
+DNAC= os.getenv("DNAC") or "172.17.3.101"
+DNAC_USER= os.getenv("DNAC_USER") or "admin"
+DNAC_PORT=os.getenv("DNAC_PORT") or 443
+DNAC_PASSWORD= os.getenv("DNAC_PASSWORD") or "CiscoLive2019"
